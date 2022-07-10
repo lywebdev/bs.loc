749d11e2062ed08c05393e3ab8f04eb7d478861f
lkwebdev <lkwebdev@mail.ru>
2022-07-10 17:46:07 +0300

    BS-2

--- resources/views/admin/components/images_loader/images_loader.blade.php
+++ resources/views/admin/components/images_loader/images_loader.blade.php
@@ -2,16 +2,20 @@
     <div class="images-loader__controls">
         <div class="images-loader__load-btn btn btn-outline-info waves-effect waves-light">Добавить фотографии</div>
     </div>
+    <div class="images-loader__images">
+
+    </div>
 </div>
 
 
 <script>
 window.addEventListener('DOMContentLoaded', () => {
     $loadBtn = $('.images-loader__load-btn');
+    $imagesContainer = $('.images-loader__images');
 
     function loadBtnInit() {
         $loadBtn.bind('click', (e) => {
-
+            
         });
     }
 });
